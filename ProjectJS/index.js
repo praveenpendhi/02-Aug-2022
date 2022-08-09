2e6262a8a8d2e1f13a3537fd15e54530f8230975
Armedscarab <praveen.pendhi@gmail.com>
2022-08-09 12:22:36 +0530

    final

--- ProjectJS/index.js
+++ ProjectJS/index.js
@@ -8,7 +8,7 @@ function validate(){
     let stateInput = document.getElementById('valstate').value
     let zipInput = document.getElementById('valzip').value
     let tncInput = document.getElementById('valtnc').checked
-
+    let error = false;
     var onlyalphabet = /^[A-Za-z]+$/;
     if(firstNameInput.match(onlyalphabet)) {
         document.getElementById('firstname-valid').style.display = 'block'
@@ -16,6 +16,7 @@ function validate(){
     } else {
         document.getElementById('firstname-valid').style.display = 'none'
         document.getElementById('firstname-invalid').style.display = 'block'
+        error = true;
     }
 
     if(lastNameInput.match(onlyalphabet)) {
@@ -24,6 +25,7 @@ function validate(){
     } else {
         document.getElementById('lastname-valid').style.display = 'none'
         document.getElementById('lastname-invalid').style.display = 'block'
+        error = true;
     }
     
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -33,31 +35,35 @@ function validate(){
       } else {
         document.getElementById("user-invalid").style.display = "block";
         document.getElementById("user-valid").style.display = "none";
+        error = true;
       }
 
-    if(cityInput.match(onlyalphabet)) {
+    if(cityInput.match(onlyalphabet) && cityInput.length >= 3) {
         document.getElementById('city-valid').style.display = 'block'
         document.getElementById('city-invalid').style.display = 'none'
     } else {
         document.getElementById('city-valid').style.display = 'none'
         document.getElementById('city-invalid').style.display = 'block'
+        error = true;
     }
 
-    if(stateInput.length >= 1) {
+    if(stateInput.length >= 1 && stateInput != "none") {
         document.getElementById('state-valid').style.display = 'block'
         document.getElementById('state-invalid').style.display = 'none'
     } else {
         document.getElementById('state-valid').style.display = 'none'
         document.getElementById('state-invalid').style.display = 'block'
+        error = true;
     }
 
     var zipformat =  /^[0-9]+$/;
-    if(zipInput.match(zipformat)){
+    if(zipInput.match(zipformat) && zipInput.length == 6){
         document.getElementById('zip-valid').style.display = 'block'
         document.getElementById('zip-invalid').style.display = 'none'
     } else {
         document.getElementById('zip-valid').style.display = 'none'
         document.getElementById('zip-invalid').style.display = 'block'
+        error = true;
     }
 
     if(tncInput) {
@@ -66,6 +72,29 @@ function validate(){
     } else {
         document.getElementById('tnc-valid').style.display = 'none'
         document.getElementById('tnc-invalid').style.display = 'block'
+        error = true;
+    }
+
+    if(error != true)
+    {
+        alert('You have succesfully registered')
+
+        document.getElementById('valfirstName').value = ''
+        document.getElementById('vallastName').value = ''
+        document.getElementById('valuserName').value = ''
+        document.getElementById('valcity').value = ''
+        document.getElementById('valstate').value = 'none'
+        document.getElementById('valzip').value = ''
+        document.getElementById('valtnc').checked = false
+
+        document.getElementById('firstname-valid').style.display = 'none'
+        document.getElementById('lastname-valid').style.display = 'none'
+        document.getElementById('user-valid').style.display = 'none'
+        document.getElementById('city-valid').style.display = 'none'
+        document.getElementById('state-valid').style.display = 'none'
+        document.getElementById('zip-valid').style.display = 'none'
+        document.getElementById('tnc-valid').style.display = 'none'
+
     }
 
 }
